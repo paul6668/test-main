6702a9e837653dcf155e69e6a376eb3464ffa1f2
Paul <105109093+paul6668@users.noreply.github.com>
2024-07-15 17:17:56 +0800

    Update starrock-os-setting.sh

--- tmp/starrock-os-setting.sh
+++ tmp/starrock-os-setting.sh
@@ -8,7 +8,7 @@ set -e
 #    --header "Cookie: oraclelicense=accept-securebackup-cookie" \
 #    http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
 
-tar -xzf openjdk-8u322-b06-linux-x64.tar.gz -C /usr/local
+tar -xzf MirrorShip-EE-3.2.8/file/openjdk-8u322-b06-linux-x64.tar.gz -C /usr/local
 JAVA_HOME=/usr/local/java-se
 
 # Set JAVA_HOME
